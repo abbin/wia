d6c22600fb8c4beac79a758fa184575122406106
abbin <helloabbin@gmail.com>
2016-11-14 09:06:16 +0530

    Initial Commit

---
+++ wia/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  wia
+//
+//  Created by Abbin Varghese on 14/11/16.
+//  Copyright © 2016 What I Ate. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
